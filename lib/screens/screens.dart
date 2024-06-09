99f58558f1fad850e0b865f6e94eea9e22904abf
Carlos Marín <cmarinbotella@gmail.com>
2024-06-09 10:20:47 +0200

    c

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -2,6 +2,5 @@ export 'package:inforas/screens/event_editing_page.dart';
 export 'package:inforas/screens/login_screen.dart';
 export 'package:inforas/screens/register_screen.dart';
 export 'package:inforas/screens/home.dart';
-export 'package:inforas/screens/documentForm.dart';
 export 'package:inforas/screens/eventForm.dart';
 export 'package:inforas/screens/calendar_screen.dart';
