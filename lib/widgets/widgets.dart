e5e3d5c0983159c1f64dbd96f1110b300932870c
Carlos Marín <cmarinbotella@gmail.com>
2024-06-09 21:03:56 +0200

    as

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,6 +1,5 @@
 export 'package:inforas/widgets/documentCard.dart';
 export 'package:inforas/widgets/cardContainer.dart';
-export 'package:inforas/widgets/authbackground.dart';
 export 'package:inforas/widgets/login_background.dart';
 export 'package:inforas/widgets/custom_appbar.dart';
 export 'package:inforas/widgets/link_to_screens.dart';
